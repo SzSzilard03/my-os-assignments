2fc5edbbd0e083d80efca926e3f821a3f678c03b
SzSzilard03 <sziliszoll54@gmail.com>
2023-05-28 00:27:08 +0300

    idk

--- a3/a3.c
+++ a3/a3.c
@@ -4,9 +4,11 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <string.h>
 #define FIFO_NAME "RESP_PIPE_10230"
 #define FIFO_OPEN "REQ_PIPE_10230"
+#define SHM_NAME "/BF2oU7W"
 
 int main(int argc, char** argv){
     int fd =-1;
@@ -18,12 +20,12 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    fd= open(FIFO_OPEN,O_RDWR);
+    fd= open(FIFO_OPEN,O_RDONLY);
     if(fd == -1){
         perror("ERROR\ncannot open request pipe");
         return 1;
     }
-    fd2= open(FIFO_NAME,O_RDWR);
+    fd2= open(FIFO_NAME,O_WRONLY);
     if(fd2 == -1){
         perror("ERROR\ncannot open response pipe");
         return 1;
@@ -37,20 +39,42 @@ int main(int argc, char** argv){
 
     char request[250];
     
-    while (1) {
+    //while (1) {
     read(fd, request, 250);    
     if(strcmp("ECHO",strtok(request,"#"))==0){
         unsigned int x = 10230;
         write(fd2,"ECHO#",strlen("ECHO#"));
         write(fd2,&x,sizeof(x));
         write(fd2,"VARIANT#",strlen("VARIANT#"));
-        break;
+        //break;
     }
     else{
-        break;
+        if(strcmp("CREATE_SHM",strtok(request,"#"))==0){
+            //create a shared memory region of 4857058 bytes
+            unsigned int size= 4857058;
+            //read(fd,&size,4);
+            
+            int shm_fd = shm_open(SHM_NAME,O_CREAT | O_RDWR,0664);
+            if(shm_fd == -1){
+                perror("ERROR\n");
+                return 1;
+            }
+            if(ftruncate(shm_fd,size) == -1){
+                perror("ERROR\n");
+                return 1;
+            }
+            write(fd2,"CREATE_SHM#",strlen("CREATE_SHM#"));
+            write(fd2,"SUCCESS#",strlen("SUCCESS#"));
+            close(shm_fd);
+            
+            
+           // break;
+        }
+        //else
+       // break;
     }
-    }
-
+   // }
+    shm_unlink(SHM_NAME);
     close(fd2);
     close(fd);
     
